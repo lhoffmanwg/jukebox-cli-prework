752e6b1d7d78fa65fc89251e4a43895c56367016
Laura Hoffman <lhoffmanwg@gmail.com>
2019-03-29 14:37:44 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,32 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following  commends:"
+  puts "help: displays this help message"
+  puts "list: displays a list of songs you can play"
+  puts "play: lets you choose a song to play"
+  puts "exit: exits this program"
+end  
+
+def play(input_from_user, songs)
+  
+  
+end
+
+def list(songs) 
+  songs.each_with_index do |song, index|
+    puts "#{index +1}. #{song}"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(argument) 
+  help
+  play
+  list
+  exit_jukebox
+end  
